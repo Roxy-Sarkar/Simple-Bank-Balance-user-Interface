cfb04af22aaba7f693670b35adbeca14ab292eb8
Roxy Sarkar <roxysarkar2030@gmail.com>
2022-11-03 18:39:11 +0600

    'Email and password' Authentication' Process running

---
+++ JS/login.js
@@ -0,0 +1,15 @@
+document.getElementById('btn-submit').addEventListener('click',function(){
+     const emailfield=document.getElementById('user-email');
+     const passwordfield=document.getElementById('user-password');
+     if(emailfield.value===""&&passwordfield.value===""){
+        window.location.href="financial_status.html";
+        
+     }
+     else{
+        alert("Please Enter Your Valid Information");
+     }
+     
+
+
+
+}) 
